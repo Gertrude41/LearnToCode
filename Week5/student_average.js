ea2469f8196065213f5b5903db6dbf98b8534ea8
Gertrude Kingunza <gertrudekingunza7@gmail.com>
2023-11-02 07:37:01 -0500

    hello

---
+++ Week5/student_average.js
@@ -0,0 +1,27 @@
+//student_average.js
+//WB4 PAGE 31 SECTION 2-17
+
+//BOOK SUGGEST TWO LOOPS
+
+//OUTER LOOP FOR STUDENTS
+//INNER LOOP FOR SCORES
+
+//IMPLAMENTATIONS
+function showALLstudentAverages(students){
+    //console.log("START"+students.length)
+
+
+
+    //TEST DATA
+    let students=[
+        {name:"Gertrude",scores:[100,96,99,92]},
+        {name:"Destiny",scores:[92,89,96,100,94]},
+        {name:"Diva",scores:[86,72,92]},
+        {name:"Geek",scores:[98,84,89,100,100,76]},
+        {name:"Bervely",scores:[89,100]},
+        {name:"Elisha",scores:[100,99,100,87]},
+    ];
+    //TEST
+    showALLstudentAverages(students) ;  
+    ]
+}
